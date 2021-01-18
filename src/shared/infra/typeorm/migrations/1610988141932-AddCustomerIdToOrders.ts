f4bc55f4130635684b47bd95e22267a8fd11f96d
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-01-18 13:50:11 -0300

    :bank: database(backend): create AddCustomerIdToOrders migration
    - Create relationship many to one between orders and customers tables;
    - Create foreign key customer_id on table orders.

---
+++ src/shared/infra/typeorm/migrations/1610988141932-AddCustomerIdToOrders.ts
@@ -0,0 +1,38 @@
+import {
+  MigrationInterface,
+  TableColumn,
+  TableForeignKey,
+  QueryRunner,
+  Table,
+} from 'typeorm';
+
+export default class AddCustomerIdToOrders1610988141932
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'customer_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrdersCustomer',
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: `SET NULL`,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrdersCustomer');
+
+    await queryRunner.dropColumn('orders', 'customer_id');
+  }
+}
